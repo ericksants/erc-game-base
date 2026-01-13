44c1bfd96ac122e8d31b4584298796a49a942717
Erick Abreu <69941187+ericksants@users.noreply.github.com>
2026-01-13 20:27:18 -0300

    First commit (Starting something)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    //Define a rota e mapeamento do componente HomeComponent para o caminho 'home'.
+    path: 'home',
+    component: HomeComponent,
+  }
+];
+
+
